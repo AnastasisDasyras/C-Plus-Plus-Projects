24f3c0c3db86a1accaaa2aa0417f8aa4a99d690b
Anastasis Dasyras <anastasisdasy@gmail.com>
2020-06-15 21:18:07 +0300

    Change cipher to ceaser cipher with a decryption function

--- DaCipher/main.cpp
+++ DaCipher/main.cpp
@@ -1,62 +1,100 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <bits/stdc++.h>
+#include <algorithm>
 
-const int MODULO
-{
-    26
-};
+const int MODULO{ 26 };
 
 using namespace std;
 
-//Function Prototype - default values in arguments in the end! ex. int modInverse(int a, int m=10);
-//Info: Pass a reference in a function with & ex. int modInverse(int &a);
-//Info: Pass a reference in a function with & but read only(const) ex. int modInverse(const int &a);
-//Info: A static variable keeps its values between function calls. Is like a global var with local visibility!
+string ceasar(string plainText,int shift);
+vector<int> str2lst(string text);
+string lst2str(vector<int> lst);
+string decrypt_ceaser(string cipher,int shift);
 
-int modInverse(int a, int m);
+// Function Prototype - default values in arguments in the end! ex. int modInverse(int a, int m=10);
+// Info: Pass a reference in a function with & ex. int modInverse(int &a);
+// Info: Pass a reference in a function with & but read only(const) ex. int modInverse(const int &a);
+// Info: A static variable keeps its values between function calls. Is like a global var with local visibility!
 
 int main()
 {
+	string cipher {};
+	string plainText {};
+	int key {};
 
-    int key_a {7};
-    int key_b {2};
-    string cipherString;
-    string plainString;
-    string plainText;
-    
-    cout << "Enter plain text for encryption:" << endl;
-    getline(cin,plainText);
-    //Break string into chars 
-    char charArray[plainText.length() + 1];
-    strcpy(charArray, plainText.c_str());
-    
-    //Encryption
-    for (auto let : plainText) {
-        char cipher = (key_a*(let-'a')+key_b) % MODULO;
-        cipherString += (cipher+'a');
-    }
-    cout <<"Cipher: " << cipherString <<endl;
-    //Decryption
-    int modInv = modInverse(key_a,MODULO);
-    cout << "ModInv: " << modInv << endl;
-    for (auto let : cipherString) {
-        char plainChar = (((let)-key_b)*modInv +1) % MODULO;
-        plainString += (plainChar+'a');
-    }
-    cout << "Plain: " << plainString <<endl;
-
-
+	cout << "Enter text for encryption: ";
+	getline(cin,plainText);
+	//All capital
+	std::transform(plainText.begin(), plainText.end(),plainText.begin(), ::toupper);
+	//delete spaces
+    plainText.erase(remove(plainText.begin(), plainText.end(), ' '), plainText.end()); 
+	cout << "Enter a key (0<integer<=25) for encrytpion:"<<endl;
+	cin >> key;
+	cout << "This is the text for encryption: " << plainText << endl;
+	cipher = ceasar(plainText,key);
+	cout <<"This is the encrypted text: "<< cipher << endl;
+	plainText = decrypt_ceaser(cipher,key);
+	cout << "This is the decryption of the cipherText: "<< plainText <<endl;
+	
+	cout << "problem: " << ((-12)%26+26)%26;
+	
     return 0;
 }
 
-int modInverse(int a, int m)
+string ceasar(string plainText, int shift)
 {
-    a = a%m;
-    for (int x=1; x<m; x++)
-        if ((a*x) % m == 1) {
-            return x;
-        }
-    return 0;
+
+    vector<int> plainTextList{};
+    plainTextList = str2lst(plainText);
+	vector<int> cipherTextList {};
+	for (auto x : plainTextList){
+		x = (x + shift) % MODULO;
+		cipherTextList.push_back(x);
+	}
+	
+	string cipher = lst2str(cipherTextList);
+	
+	return cipher;
+	
+}
+
+string decrypt_ceaser(string cipher,int shift){
+	
+	vector<int> cipherTextList {str2lst(cipher)};
+	vector<int> plainTextList {};
+	//mod (%) OPERATION IN C++ IS JUST THE REMINDER NOT THE MODULO IN A DACTYLUS
+	for (auto c : cipherTextList){
+		c = (((c-shift) % MODULO) + MODULO) % MODULO;
+		plainTextList.push_back(c);
+	}
+	
+	string plainText {lst2str(plainTextList)};
+	
+	return plainText;
+	
+}
+
+vector<int> str2lst(string text)
+{
+
+    vector<int> plainTextList{};
+    for(auto t : text) {
+		plainTextList.push_back((int)t-65);
+    }
+
+	for (auto i : plainTextList)
+		cout << i << " ";
+    return plainTextList;
+}
+
+string lst2str(vector<int> lst)
+{
+
+    string str {};
+    for(auto l : lst) {
+		str += char(l+65);
+    }
+
+    return str;
 }
